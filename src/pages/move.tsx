325ab1c58b025ac8b2271a2558da3aeaadd17c04
Toki <55911995+tokio-k@users.noreply.github.com>
2021-03-16 15:09:01 +0900

    add/move

--- src/pages/move.tsx
+++ src/pages/move.tsx
@@ -5,7 +5,18 @@ export default function Move() :JSX.Element {
     return (
         <MainLayout>
         <Headline title={"動画紹介ページ"}/>
-        現在作成中です。
+        動画紹介ページ作成中
+        <div>
+        <video controls width="200">
+            <source src="/img/637565696.466592.mp4" type="video/mp4"/>
+        </video>
+        <video controls width="200">
+            <source src="/img/637565696.641354.mp4" type="video/mp4"/>
+        </video>
+        <video controls width="200">
+            <source src="/img/637565696.759496.mp4" type="video/mp4"/>
+        </video>
+        </div>
         </MainLayout>
     )
 }
