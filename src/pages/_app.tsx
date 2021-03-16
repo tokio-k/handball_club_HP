b8fe06c37de16068d71af0102bcd6c35e59e2dcc
Toki <55911995+tokio-k@users.noreply.github.com>
2021-03-17 06:38:04 +0900

    fix/move

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.scss'
 import { AppProps } from 'next/app'
 
+
 export default function App({ Component, pageProps }: AppProps) :JSX.Element {
   return <Component {...pageProps} />
 }
